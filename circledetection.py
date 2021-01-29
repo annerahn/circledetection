0da3fa5a0be9d7f2369c9b2f0d22d4e9ef153459
Tim Siefert <Tim.Siefert@ifm.com>
2021-01-29 10:34:24 +0100

    port 8000

--- circledetection.py
+++ circledetection.py
@@ -84,4 +84,4 @@ def detection():
     return Response(json.dumps(output_dict, indent=4), mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
